739bc705a9e7a21ec1616a331f99143250f0203e
Mazi Prima Reza <mazi.reza@vidio.com>
2023-06-30 01:25:25 +0700

    add filter project button

---
+++ assets/scripts/active-button.js
@@ -0,0 +1,10 @@
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("btn active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
