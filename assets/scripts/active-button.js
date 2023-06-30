13e376751689896c3e870e275bfa463f0fd3f330
Mazi Prima Reza <mazi.reza@vidio.com>
2023-06-30 10:01:48 +0700

    fix active button in filter project

--- assets/scripts/active-button.js
+++ assets/scripts/active-button.js
@@ -1,9 +1,9 @@
 // Add active class to the current control button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+var btns = btnContainer.getElementsByClassName("btnf");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("btn active");
+    var current = document.getElementsByClassName("btnf active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
