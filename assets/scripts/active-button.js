5bdb2e5387315cc4bf71144aa500acf3e3efcee2
Mazi Prima Reza <mazi.reza@vidio.com>
2023-06-30 10:54:10 +0700

    add responsive to project page and tooltip

--- assets/scripts/active-button.js
+++ assets/scripts/active-button.js
@@ -8,3 +8,9 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+// tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
